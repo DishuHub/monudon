9fce0872251ea93c52c53344b24033d0a53eca20
DishuHub <dikshant00tanwar@gmail.com>
2023-03-21 14:36:06 +0530

    second comment

--- MONU.cpp
+++ MONU.cpp
@@ -1,7 +1,20 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
+int swap(int a,int b)
+{
+    int temp; 
+    temp = a;
+    a = b;
+    b = temp;
+    cout<<a<<endl<<b;
+    return 0;
+}
 int main()
 {
-    cout << "Hello World!!" ;
+    int a = 10;
+    int b = 15;
+
+    swap(a,b);
+    cout << endl<< "Hello World!!" ;
     return 0;
 }
